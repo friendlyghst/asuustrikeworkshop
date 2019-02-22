2ce075348eeba01429542ce1ce45c76b3789c0e0
Gbenga <omg.tosine@gmail.com>
2019-02-22 07:07:21 +0100

    Update test.py

--- python/team B/test.py
+++ python/team B/test.py
@@ -1,7 +1,13 @@
 from QUESTION import question
 
 class evaluate:
+    """ Contains evaluate_response(response,answer) 
+    the function takes in two arguments: response, answer and compares the two to calculae a score.
+    """
+
     def evaluate_response(self, response, answer):
+        # checking answer for a match with the default response to question
+        # and returns the score based on comparisons
         if response.upper() == answer:
             score = 10
         else:
