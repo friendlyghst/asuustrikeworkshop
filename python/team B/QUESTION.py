04c9eba0edccc982b39bbea4bb7e6e8ca7aed712
Gbenga <omg.tosine@gmail.com>
2019-02-22 06:42:23 +0100

    Update QUESTION.py

--- python/team B/QUESTION.py
+++ python/team B/QUESTION.py
@@ -1,5 +1,8 @@
 #question object
 class question:
+    """ Contains a function that takes in an input question as an argument
+    and returns the question as a response"""
+    
     def ask_question(self,question):
         response = input(question)
         return response
