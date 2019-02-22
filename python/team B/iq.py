76b5e31cadfd1240cb2984c935d94805abda0c4a
Gbenga <omg.tosine@gmail.com>
2019-02-22 06:59:27 +0100

    Update iq.py

--- python/team B/iq.py
+++ python/team B/iq.py
@@ -3,25 +3,36 @@ from test import evaluate
 from newfile import enoch
 
 class Question:
+    """ Contains quest(). A function that contains the list
+    of questions to be asked
+    """
+    
     def quest(self):
         
         print("Pick an Option")
-
+        
+        # a list of questions
         list = [
         "Oliver Twist was published by who? (A)  Oliver Twist, (B) Charles Dickens: ",
         "What was the name of Alexander the great's horse? (A) Alonso, (B) Bucaphalus: ",
         "According to greek mythology, who is the god of thunder? (A) Ogun, (B) Thor: "
         ]
-
+        
+        # an empty list to store the results of each questions asked
         score = []
         
+        # a loop that iterates through the elements in the list of questions
         for el in list:
-            question1 = question.ask_question(el)
-            scoree = evaluate.evaluate_response(question1,"B")
-            score.append(scoree)
+            question1 = question.ask_question(el)   # calls the ask_question() function from the question class in QUESTION
+            scoree = evaluate.evaluate_response(question1,"B")  # calls the evaluate_response() function from the evaluate class in test 
+            score.append(scoree)   #append the result of the evaluate_response() function to score
+        
+        # prints the score of each question on the console
         print(score)
+        # returns the sum of elements in score
         tiq = sum(score)
-
+         
+        # displays the iq score on the console
         print("Your IQ is:", tiq)        
 
         
